1f420b24a51de480ce30d4f3cd86c05e41139d45
akshayiic <akshay.pednekar.iic.0118@gmail.com>
2025-11-03 10:59:16 +0530

    update : changed theme

--- src/routes/Home.svelte
+++ src/routes/Home.svelte
@@ -50,58 +50,6 @@
 	});
 </script>
 
-{#if !$currentUI.highlights && false}
-	<button
-		id="daynighmode"
-		type="button"
-		class="day-night-toggle"
-		on:click={toggleDayNight}
-		style="position: fixed; top: 3rem; right: 2rem; cursor: pointer; z-index: 1000001; background: white; padding: .4rem; border-radius: 50%;"
-		aria-label="Toggle day and night mode"
-	>
-		{#if $isDay}
-			<!-- Replace with lucide Sun icon -->
-			<svg
-				xmlns="http://www.w3.org/2000/svg"
-				class="icon icon-sun"
-				width="24"
-				height="24"
-				viewBox="0 0 24 24"
-				fill="none"
-				stroke="currentColor"
-				stroke-width="2"
-				stroke-linecap="round"
-				stroke-linejoin="round"
-			>
-				<circle cx="12" cy="12" r="5"></circle>
-				<line x1="12" y1="1" x2="12" y2="3"></line>
-				<line x1="12" y1="21" x2="12" y2="23"></line>
-				<line x1="4.22" y1="4.22" x2="5.64" y2="5.64"></line>
-				<line x1="18.36" y1="18.36" x2="19.78" y2="19.78"></line>
-				<line x1="1" y1="12" x2="3" y2="12"></line>
-				<line x1="21" y1="12" x2="23" y2="12"></line>
-				<line x1="4.22" y1="19.78" x2="5.64" y2="18.36"></line>
-				<line x1="18.36" y1="5.64" x2="19.78" y2="4.22"></line>
-			</svg>
-		{:else}
-			<!-- Replace with lucide Moon icon -->
-			<svg
-				xmlns="http://www.w3.org/2000/svg"
-				class="icon icon-moon"
-				width="24"
-				height="24"
-				viewBox="0 0 24 24"
-				fill="none"
-				stroke="currentColor"
-				stroke-width="2"
-				stroke-linecap="round"
-				stroke-linejoin="round"
-			>
-				<path d="M21 12.79A9 9 0 0111.21 3 7 7 0 1012 21a9 9 0 009-8.21z"></path>
-			</svg>
-		{/if}
-	</button>
-{/if}
 <div
 	class={$hotspotName == 'Exterior'
 		? 'cloudimage-360 exterior-cloudi !absolute left-0 top-0  z-[10]'
@@ -115,21 +63,6 @@
 	data-request-responsive-images="true"
 	data-info="false"
 	data-ratio=" 1"
-></div>
-
-<div
-	class={$hotspotName == 'night'
-		? 'cloudimage-360 exterior-cloudi !absolute left-0 top-0  z-[10]'
-		: 'cloudimage-360 invisible'}
-	data-folder="https://assets.vestate.io/runwal-kane/exterior/night/"
-	data-filename="{'night{'}index{'}'}.webp"
-	data-amount="24"
-	data-keys="false"
-	data-filters="blur:20"
-	data-drag-speed="400"
-	data-request-responsive-images="true"
-	data-info="false"
-	data-ratio="1"
 ></div>
 
 <style>
--- src/routes/vicinities/+page.svelte
+++ src/routes/vicinities/+page.svelte
@@ -44,7 +44,7 @@
 	}
 </script>
 
-<div class="left-panel-wrapper">
+<!-- <div class="left-panel-wrapper">
 	<div class="left-panel p-2">
 		<div class="left-panel--header flex justify-between gap-[2rem] lg:gap-[5rem]">
 			<div class="left-title flex items-center font-bold">
@@ -58,11 +58,11 @@
 				>
 					<path
 						d="M8.49535 0.0693359C11.2918 0.0693359 13.4186 2.10511 13.4992 4.65137C13.524 5.34636 13.3008 5.986 13.0341 6.60718C12.3087 8.32929 11.2918 9.87918 10.2315 11.4106C9.79127 12.0441 9.32622 12.653 8.87359 13.2803C8.78058 13.4095 8.71857 13.4464 8.60696 13.2926C7.05063 11.2077 5.5687 9.07964 4.46501 6.71174C3.37992 4.38075 4.31 2.43723 5.4943 1.34246C6.40578 0.499862 7.49087 0.0877871 8.49535 0.0693359ZM8.74338 6.36117C9.61145 6.36117 10.3183 5.66618 10.3245 4.80512C10.3307 3.94407 9.60525 3.21833 8.73717 3.21833C7.8691 3.21833 7.14364 3.95022 7.15604 4.81128C7.16844 5.67233 7.8753 6.36117 8.74338 6.36117Z"
-						fill="#5A4DE3"
+						fill="#A47764"
 					/>
 					<path
 						d="M8.29317 14.232C10.3207 14.2258 11.8585 14.0167 13.3218 13.414C13.8488 13.1987 14.3511 12.9342 14.7541 12.5222C15.2006 12.067 15.1944 11.6611 14.7417 11.2121C14.3015 10.7754 13.7434 10.5233 13.173 10.308C12.9808 10.2342 12.9684 10.1789 13.0738 10.0128C13.2784 9.68068 13.4582 9.33626 13.6442 8.99184C13.7124 8.86883 13.762 8.80118 13.9232 8.86883C14.6735 9.1948 15.3866 9.58842 15.9508 10.1912C16.9305 11.2429 16.9243 12.6021 15.9198 13.6354C15.1199 14.4534 14.1031 14.9085 13.018 15.2037C9.99211 16.034 6.96625 16.0832 3.9652 15.0684C3.15913 14.7978 2.41506 14.398 1.77641 13.826C0.4805 12.6513 0.474299 11.1445 1.75781 9.9513C2.28485 9.46542 2.89871 9.11485 3.54356 8.80733C3.71098 8.72737 3.76678 8.78272 3.84119 8.92418C4.0272 9.2809 4.21322 9.63148 4.42404 9.96975C4.51704 10.1235 4.47984 10.1666 4.33103 10.2281C3.89079 10.4126 3.46915 10.6278 3.08472 10.9046C2.97931 10.9846 2.8739 11.0707 2.7747 11.1629C2.25385 11.6611 2.24765 12.0916 2.7809 12.5775C3.52496 13.2602 4.44884 13.58 5.40372 13.8322C6.50741 14.1274 7.62351 14.2443 8.29317 14.232Z"
-						fill="#5A4DE3"
+						fill="#A47764"
 					/>
 				</svg>
 				Vicinities
@@ -95,7 +95,9 @@
 						</Accordion.Item>
 
 						<Accordion.Item value="transport">
-							<Accordion.Trigger id="transport-level">Connectivity</Accordion.Trigger>
+							<Accordion.Trigger id="transport-level" class="acc-label"
+								>Connectivity</Accordion.Trigger
+							>
 							<Accordion.Content>
 								{#each [{ id: 'Sakinaka Metro Station.webp', label: 'Sakinaka Metro Station' }, { id: 'Andheri Kurla Road.webp', label: 'Andheri Kurla Road' }, { id: 'Upcoming Rambaug Metro Station.webp', label: 'Upcoming Rambaug Metro Station' }, { id: 'JVLR.webp', label: 'JVLR' }, { id: 'Ghatkopar Railway Station.webp', label: 'Ghatkopar Railway Station' }, { id: 'Aarey Aqua Metro Station.webp', label: 'Aarey Aqua Metro Station' }, { id: 'SEEPZ METRO.webp', label: 'SEEPZ Metro' }, { id: 'T2 International Airport.webp', label: 'T2 International Airport' }, { id: 'LBS Road - Vikhroli.webp', label: 'LBS Road - Vikhroli' }, { id: 'MIDC METRO.webp', label: 'MIDC Metro' }, { id: 'Eastern Express Highway.webp', label: 'Eastern Express Highway' }, { id: 'Andheri Railway Station.webp', label: 'Andheri Railway Station' }, { id: 'Western Express Highway.webp', label: 'Western Express Highway' }, { id: 'Ghatkopar Mankhurd Link Road.webp', label: 'Ghatkopar Mankhurd Link Road' }, { id: 'Bandra Worli Sea Link.webp', label: 'Bandra Worli Sea Link' }] as scene}
 									<button
@@ -234,7 +236,7 @@
 			</div>
 		</div>
 	</div>
-</div>
+</div> -->
 
 <div class={'d-block visible absolute bottom-0 left-0 right-0 top-0'}>
 	<div
